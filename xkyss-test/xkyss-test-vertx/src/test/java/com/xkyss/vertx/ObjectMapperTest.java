62eb4db2b9778f850a5f574c43d2b6050ce3ed48
xkyii <xkyii@foxmail.com>
2025-03-10 20:58:41 +0800

    Add: OjbectMapper 自定义类序列化器 测试

--- xkyss-test/xkyss-test-vertx/src/test/java/com/xkyss/vertx/ObjectMapperTest.java
+++ xkyss-test/xkyss-test-vertx/src/test/java/com/xkyss/vertx/ObjectMapperTest.java
@@ -1,14 +1,20 @@
 package com.xkyss.vertx;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.jackson.DatabindCodec;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.atomic.LongAdder;
 
 public class ObjectMapperTest {
     @Test
@@ -34,10 +40,48 @@ public class ObjectMapperTest {
         Assertions.assertTrue(map.containsKey("code.key"));
     }
 
+    @Test
+    public void test_03() {
+
+        ObjectMapper om = DatabindCodec.mapper();
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        om.findAndRegisterModules();
+        om.registerModule(new TestModule());
+
+        Bar bar = new Bar();
+        bar.setCounter(new LongAdder());
+        bar.getCounter().increment();
+
+        {
+            String s = Json.encode(bar);
+            Assertions.assertEquals("{\"counter\":1}", s);
+
+            Bar b = Json.decodeValue(s, Bar.class);
+            Assertions.assertNotNull(b);
+        }
+
+        {
+            JsonObject jo = JsonObject.mapFrom(bar);
+            Assertions.assertEquals("{\"counter\":1}", jo.toString());
+
+            Bar b = jo.mapTo(Bar.class);
+            Assertions.assertNotNull(b);
+            Assertions.assertEquals(1L, b.getCounter().sum());
+        }
+
+    }
+
+    static class TestModule extends SimpleModule {
+        public TestModule() {
+            addSerializer(Bar.class, new BarSerializer());
+            addDeserializer(Bar.class, new BarDeserializer());
+        }
+    }
+
 
     static class Foo {
         public Foo() {
-
         }
 
         public Foo(String name, String code) {
@@ -46,5 +90,44 @@ public class ObjectMapperTest {
         }
         public String nameKey;
         public String codeKey;
+    }
+
+    static class Bar {
+        private LongAdder counter;
+
+        public LongAdder getCounter() {
+            return counter;
+        }
+
+        public void setCounter(LongAdder counter) {
+            this.counter = counter;
+        }
+    }
+
+    static class BarSerializer extends JsonSerializer<Bar> {
+        @Override
+        public void serialize(Bar bar, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeNumberField("counter", bar.getCounter().sum());
+            jsonGenerator.writeEndObject();
+        }
+    }
+
+    static class BarDeserializer extends JsonDeserializer<Bar> {
+
+        @Override
+        public Bar deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+            Bar bar = new Bar();
+            bar.setCounter(new LongAdder()); // 初始化 LongAdder 对象
+
+            // 解析 JSON 数据
+            JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            if (node.has("counter")) {
+                long counterValue = node.get("counter").asLong(); // 获取 counter 字段的值
+                bar.getCounter().add(counterValue); // 将值添加到 LongAdder 对象中
+            }
+
+            return bar;
+        }
     }
 }
